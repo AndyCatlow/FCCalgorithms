8d76811b3b6623ba1addfbca8e9aa37c3f5417b6
AndyCatlow <mrcatlow@gmail.com>
2021-04-26 22:29:10 +0200

    add problem skeleton

--- intermediate/spinal-case.js
+++ intermediate/spinal-case.js
@@ -1,2 +1,9 @@
 // Spinal Tap Case
 // Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+function spinalCase(str) {
+  let strArr = str.split(/^([a-b])([A-Z])/);
+  // let spinal = str.replace(/([A-Z])/g, "-").toLowerCase();
+  console.log(strArr);
+}
+
+spinalCase("This Is Spinal Tap");
