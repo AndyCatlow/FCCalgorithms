99353d2be3866d1f6b79872895f06ecae8d3670f
AndyCatlow <mrcatlow@gmail.com>
2021-05-10 17:47:09 +0200

    fix string bug

--- intermediate/SearchAndReplace.js
+++ intermediate/SearchAndReplace.js
@@ -10,17 +10,24 @@
 
 function myReplace(str, before, after) {
   let result = str.search(before);
+
   console.log(result);
+
   console.log(str[result]);
+
   if (str[result].match(/[A-Z]/)) {
     after = after.charAt(0).toUpperCase() + after.slice(1);
   } else {
     console.log("lower");
   }
+
   console.log(after);
-  str.replace(before, after);
+
+  str = str.replace(before, after);
+
   console.log(str);
+
   return str;
 }
 
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped");
