e4157a184cb28d9274979a7c8c939e6a4f20e730
AndyCatlow <mrcatlow@gmail.com>
2021-05-11 21:41:19 +0200

    add drguildo solution

--- intermediate/DNApairs.js
+++ intermediate/DNApairs.js
@@ -48,3 +48,18 @@ function pairElement(str) {
 
 //test here
 pairElement("GCG");
+
+// drguildo
+// Dec '16
+function pairElement(str) {
+  var pairs = {
+    A: ["A", "T"],
+    T: ["T", "A"],
+    C: ["C", "G"],
+    G: ["G", "C"],
+  };
+
+  return str.split("").map(function (elem) {
+    return pairs[elem];
+  });
+}
