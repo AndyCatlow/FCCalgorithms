e156cec4d6254c1f287ce86ff5385a1ca1cdf6b7
AndyCatlow <mrcatlow@gmail.com>
2021-05-28 21:51:51 +0200

    add pseudo code

--- intermediate/sumfib.js
+++ intermediate/sumfib.js
@@ -4,3 +4,22 @@
 // The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
 
 // For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+
+function sumFibs(num) {
+  if (num === 0) {
+    return (num = 0);
+  } else {
+    let fibnums = [1];
+
+    // put fibnums into array up to value of num
+    for (i = 1; i < num; i++) {
+      // filter out even numbers
+
+      //  sum remaining fibnums
+
+      return num;
+    }
+  }
+}
+
+sumFibs(4);
