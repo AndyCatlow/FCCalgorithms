a4418bcdedfafca3f68be5fab17bd6491e0fd66d
AndyCatlow <mrcatlow@gmail.com>
2021-05-30 22:59:30 +0200

    add fib sequence generation function

--- intermediate/sumfib.js
+++ intermediate/sumfib.js
@@ -23,3 +23,13 @@ function sumFibs(num) {
 }
 
 sumFibs(4);
+
+function fib(n) {
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  console.log(arr);
+  arr.filter();
+}
+fib(5);
