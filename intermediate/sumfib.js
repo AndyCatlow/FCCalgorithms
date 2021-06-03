db20ddaf58a2b60932aa5d82b3fddabf161f1b43
AndyCatlow <mrcatlow@gmail.com>
2021-06-03 22:19:17 +0200

    add for loop solution

--- intermediate/sumfib.js
+++ intermediate/sumfib.js
@@ -6,30 +6,38 @@
 // For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
 
 function sumFibs(num) {
-  if (num === 0) {
-    return (num = 0);
-  } else {
-    let fibnums = [1];
-
-    // put fibnums into array up to value of num
-    for (i = 1; i < num; i++) {
-      // filter out even numbers
-
-      //  sum remaining fibnums
-
-      return num;
-    }
+  if (num < 2) {
+    return console.log(num);
   }
+  let fibArr = fib(num);
+  // console.log(fibArr)
+
+  function checkSize(fibNum) {
+    return fibNum <= num && fibNum % 2 != 0;
+  }
+
+  let fibOdd = fibArr.filter(checkSize);
+  // console.log(fibOdd)
+
+  return sum(fibOdd);
+
+  //  fibOdd.reduce()
 }
 
-sumFibs(4);
+sumFibs(75025);
 
 function fib(n) {
   let arr = [0, 1];
   for (let i = 2; i < n + 1; i++) {
     arr.push(arr[i - 2] + arr[i - 1]);
   }
-  console.log(arr);
-  arr.filter();
+  return arr;
 }
-fib(5);
+
+function sum(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
