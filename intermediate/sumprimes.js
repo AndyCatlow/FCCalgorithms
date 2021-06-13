3008d64277d0c546515ecb872922d596a8ceb6ba
AndyCatlow <mrcatlow@gmail.com>
2021-06-13 22:44:23 +0200

    add second solution code to sumprimes

--- intermediate/sumprimes.js
+++ intermediate/sumprimes.js
@@ -3,19 +3,53 @@
 
 // Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
 
+// function sumPrimes(num) {
+//   let primes = [];
+//   let odd = [];
+//   for (let i = 1; i <= num; i++) {
+//     if (i === 2) {
+//       primes.push(i);
+//     } else if (num % i !== 0) {
+//       primes.push(i);
+//     }
+//   }
+//   for (let i = 0; i < odd.length; i++) {}
+//   return primes;
+//   // return primes.reduce((a,b) => {return a + b});
+// }
+
+// console.log(sumPrimes(13));
+
 function sumPrimes(num) {
   let primes = [];
-  let odd = [];
-  for (let i = 1; i <= num; i++) {
-    if (i === 2) {
-      primes.push(i);
-    } else if (num % i !== 0) {
+
+  for (let i = 0; i <= num; i++) {
+    if (isPrime(i)) {
       primes.push(i);
     }
   }
-  for (let i = 0; i < odd.length; i++) {}
-  return primes;
-  // return primes.reduce((a,b) => {return a + b});
+
+  function isPrime(num) {
+    if (num < 2) return false;
+
+    /**
+     * An integer is prime if it is not divisible by any prime less than or equal to its square root
+     **/
+
+    var q = Math.floor(Math.sqrt(num));
+
+    for (var i = 2; i <= q; i++) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  return primes.reduce((a, b) => {
+    return a + b;
+  });
 }
 
-console.log(sumPrimes(13));
+sumPrimes(977);
