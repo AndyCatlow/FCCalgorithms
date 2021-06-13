76173ddc1241eba4976e5a0fa43b13d291b4277d
AndyCatlow <mrcatlow@gmail.com>
2021-06-13 22:46:56 +0200

    add sample solution using .every

--- intermediate/sumprimes.js
+++ intermediate/sumprimes.js
@@ -53,3 +53,13 @@ function sumPrimes(num) {
 }
 
 sumPrimes(977);
+
+// sample solution
+function sumPrimes(num) {
+  // Check all numbers for primality
+  let primes = [];
+  for (let i = 2; i <= num; i++) {
+    if (primes.every((prime) => i % prime !== 0)) primes.push(i);
+  }
+  return primes.reduce((sum, prime) => sum + prime, 0);
+}
