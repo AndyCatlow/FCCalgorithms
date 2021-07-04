e72d9bdb715857e59de94d3a5dd02337475f77f5
AndyCatlow <mrcatlow@gmail.com>
2021-07-04 22:07:14 +0200

    add brute force steamroller solution

--- intermediate/Steamroller.js
+++ intermediate/Steamroller.js
@@ -2,7 +2,10 @@
 // Flatten a nested array. You must account for varying levels of nesting.
 
 function steamrollArray(arr) {
-  return arr;
+  let flattened = [].concat(...arr);
+  let flattened2 = [].concat(...flattened);
+  let flattened3 = [].concat(...flattened2);
+  return flattened3;
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
