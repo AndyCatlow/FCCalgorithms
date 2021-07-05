970677b6955591092fc43d6b9613e45b8f1001ae
AndyCatlow <mrcatlow@gmail.com>
2021-07-05 10:45:04 +0200

    change flattened declaration point

--- intermediate/Steamroller.js
+++ intermediate/Steamroller.js
@@ -1,11 +1,21 @@
 // Steamroller
 // Flatten a nested array. You must account for varying levels of nesting.
 
+// function steamrollArray1(arr) {
+//   let flattened = [].concat(...arr);
+//   let flattened2 = [].concat(...flattened);
+//   let flattened3 = [].concat(...flattened2);
+//   return flattened3;
+// }
+
 function steamrollArray(arr) {
-  let flattened = [].concat(...arr);
-  let flattened2 = [].concat(...flattened);
-  let flattened3 = [].concat(...flattened2);
-  return flattened3;
+  for (let i = 0; i < arr.length; i++) {
+    let flattened = [];
+    if (Array.isArray(arr[i])) {
+      flattened = flattened.concat(...arr);
+      console.log(flattened);
+    }
+  }
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
