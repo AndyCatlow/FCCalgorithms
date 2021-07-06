d55d4b101b582abe687c1c7990ddb88dcb81505b
AndyCatlow <mrcatlow@gmail.com>
2021-07-06 10:25:35 +0200

    add recursive steamroller solution

--- intermediate/Steamroller.js
+++ intermediate/Steamroller.js
@@ -7,15 +7,27 @@
 //   let flattened3 = [].concat(...flattened2);
 //   return flattened3;
 // }
+// does not flatten more then one level deep
+// function steamrollArray(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     let flattened = [];
+//     if (Array.isArray(arr[i])) {
+//       flattened = flattened.concat(...arr);
+//       console.log(flattened);
+//     }
+//   }
+// }
 
-function steamrollArray(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let flattened = [];
-    if (Array.isArray(arr[i])) {
-      flattened = flattened.concat(...arr);
-      console.log(flattened);
+const steamrollArray = (arr) => {
+  let flattened = [];
+  arr.forEach((element) => {
+    if (Array.isArray(element)) {
+      flattened.push(...steamrollArray(element));
+    } else {
+      flattened.push(element);
     }
-  }
-}
+  });
+  return flattened;
+};
 
 steamrollArray([1, [2], [3, [[4]]]]);
