12a227b800b60d9ac32463bf7e3e5e60b2d72c88
AndyCatlow <mrcatlow@gmail.com>
2021-07-08 21:38:35 +0200

    add for loop solution

--- intermediate/EverythingBeTrue.js
+++ intermediate/EverythingBeTrue.js
@@ -7,7 +7,14 @@
 
 // Remember, you can access object properties through either dot notation or [] notation.
 
-function truthCheck(collection, pre) {}
+function truthCheck(collection, pre) {
+  for (let i = 0; i < collection.length; i++) {
+    if (!collection[i][pre]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 truthCheck(
   [
