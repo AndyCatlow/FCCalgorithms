3891101470f77a8e1d94fbc58a82147878d54c83
AndyCatlow <mrcatlow@gmail.com>
2021-11-08 17:14:04 +0200

    add make a person solution

--- intermediate/MakeAPerson.js
+++ intermediate/MakeAPerson.js
@@ -20,3 +20,32 @@
 
 //   const bob = new Person('Bob Ross');
 //   bob.getFullName();
+
+// const Person = function(firstAndLast) {
+//     let fullName = firstAndLast
+//     // Only change code below this line
+//     // Complete the method below and implement the others similarly
+//     this.getFirstName = function(){
+//       return fullName.split(" ")[0]
+//     };
+//     this.getLastName = function(){
+//       return fullName.split(" ")[1]
+//     };
+//     this.getFullName = function(){
+//       return fullName
+//     };
+//     this.setFirstName = function(first){
+//      fullName = first + fullName.split(" ")[1]
+//     };
+//     this.setLastName = function(last){
+//       fullName = fullName.split(" ")[0] + last
+//     };
+//     this.setFullName = function(firstAndLast){
+//       fullName = firstAndLast
+//     };
+
+//     return firstAndLast;
+//   };
+
+//   const bob = new Person('Bob Ross');
+//   bob.getFullName();
