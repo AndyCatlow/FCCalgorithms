ff992a18d9e0da86c39dc81f50fee1e28bbfd9a6
AndyCatlow <mrcatlow@gmail.com>
2021-11-09 11:50:44 +0200

    add roman numeral converter solution

--- intermediate/RomanNumeralConverter.js
+++ intermediate/RomanNumeralConverter.js
@@ -2,3 +2,33 @@
 // Convert the given number into a roman numeral.
 
 // All roman numerals answers should be provided in upper-case.
+
+function convertToRoman(num) {
+  const lookup = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let roman = "";
+  for (let i in lookup) {
+    while (num >= lookup[i]) {
+      roman += i;
+      num -= lookup[i];
+    }
+  }
+
+  console.log(roman);
+  return roman;
+}
+
+convertToRoman(36);
