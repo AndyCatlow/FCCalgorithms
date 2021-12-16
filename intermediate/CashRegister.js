9765dc492ec8d756efc2dd915cab7b3e63993acb
AndyCatlow <mrcatlow@gmail.com>
2021-12-16 13:34:40 +0200

    add cash register solution

--- intermediate/CashRegister.js
+++ intermediate/CashRegister.js
@@ -34,3 +34,63 @@
 //   ["TWENTY", 60],
 //   ["ONE HUNDRED", 100]
 // ]
+
+function checkCashRegister(price, cash, cid) {
+  price = price * 100;
+  cash = cash * 100;
+  let changeNeeded = cash - price;
+  // console.log(changeNeeded)
+  let cidCents = cid.map(([a, b]) => [a, Math.round(b * 100)]).reverse();
+  // console.log(cidCents)
+  let cidTotal = 0;
+  for (let amount of cidCents) {
+    cidTotal += amount[1];
+  }
+  // cidCents.map(([a,b]) => b).reduce((a, b) => a + b)
+  // console.log(cidTotal)
+  if (cidTotal < changeNeeded) {
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
+  } else if (cidTotal === changeNeeded) {
+    return { status: "CLOSED", change: cid };
+  } else {
+    const currency = {
+      "ONE HUNDRED": 10000,
+      TWENTY: 2000,
+      TEN: 1000,
+      FIVE: 500,
+      ONE: 100,
+      QUARTER: 25,
+      DIME: 10,
+      NICKEL: 5,
+      PENNY: 1,
+    };
+    let changeGiven = [];
+    for (let elem of cidCents) {
+      let holder = [elem[0], 0];
+      while (changeNeeded >= currency[elem[0]] && elem[1] > 0) {
+        changeNeeded -= currency[elem[0]];
+        elem[1] -= currency[elem[0]];
+        holder[1] += currency[elem[0]] / 100;
+      }
+      if (holder[1] > 0) {
+        changeGiven.push(holder);
+      }
+    }
+    if (changeNeeded > 0) {
+      return { status: "INSUFFICIENT_FUNDS", change: [] };
+    } else {
+      return { status: "OPEN", change: changeGiven };
+    }
+  }
+}
+checkCashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]);
